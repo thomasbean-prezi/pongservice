2e0b681b4e3bce15a95b22593423b2179f94273a
Thomas Bean <thomas.bean@prezi.com>
2018-02-14 15:03:45 +0100

    catching up frm never commitng

--- main/models.py
+++ main/models.py
@@ -11,11 +11,11 @@ class Field(models.Model):
 class Match(models.Model):
     id = models.AutoField(primary_key=True)
     date_and_time = models.DateField()
-    # player1 = models.ForeignKey(Player, on_delete=models.CASCADE) #I think this may be wrong...
-    # player2 = models.ForeignKey(Player, on_delete=models.CASCADE) #Many players to many matches
+    player1 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="player1") #I think this may be wrong...
+    player2 = models.ForeignKey(Player, on_delete=models.CASCADE, related_name="player2") #Many players to many matches
     # player1 = models.CharField(max_length=200)
     # player2 = models.CharField(max_length=200)
-    player1 = models.ManyToManyField(Player, related_name="player1")
-    player2 = models.ManyToManyField(Player, related_name="player2")
+    # player1 = models.ManyToManyField(Player, related_name="player1")
+    # player2 = models.ManyToManyField(Player, related_name="player2")
     field = models.ForeignKey(Field, on_delete=models.CASCADE) #what does this mean.. on delete cascade
     result = models.IntegerField() #store the result in a json object...?
--- main/views.py
+++ main/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
-# from django.template import loader
 
 from .models import Player
 
 def index(request):
     players = Player.objects.all()
-    # template = loader.get_template('main/index.html')
     context = {
-        'players': players, #is this how you pass the 'player' variable to the index?
+        'players': players, # must pyhton dictionary
     }
     return render(request, 'main/index.html', context)
-    # return HttpResponse(template.render(context, request))
-    #return HttpResponse("woo. you are at the main page. exciting stuff")
 
-# Create your views here.
+
