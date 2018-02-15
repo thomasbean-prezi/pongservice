77514d6330c601fc3764df395adf2bbd936f75e8
Thomas Bean <thomas.bean@prezi.com>
2018-02-15 11:15:19 +0100

    created player_detail view for examining each player

--- main/urls.py
+++ main/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='main'),
+    url(r'^(?P<player_id>[0-9]+)/$', views.player_detail, name='player_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,22 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.http import HttpResponse
 
 from .models import Player
 
 def index(request):
     players = Player.objects.all()
     context = {
-        'players': players, # must pyhton dictionary
+        'players': players,
     }
     return render(request, 'main/index.html', context)
+
+def player_detail(request, player_id):
+    try:
+        player = Player.objects.get(pk=player_id)
+    except Player.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'main/detail.html', {'player': player})
+    #return HttpResponse("You're looking at palyer %s." % player_id)
 
 
