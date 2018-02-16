ae2ba692465bb200086bc4ab852dc584f03df21a
Thomas Bean <thomas.bean@prezi.com>
2018-02-16 17:49:24 +0100

    completed all api endpoints

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url(r'^players/(?P<player_id>[0-9]+)/$', views.player_detail, name='player_detail'),
     url(r'^fields/$', views.fields, name='fields'),
     url(r'^api/players/$', views.api_players, name='api_players'),
+    url(r'^api/fields/$', views.api_fields, name='api_fields'),
+    url(r'^api/matches/$', views.api_matches, name='api_matches'),
     url(r'^api/players/(?P<player_id>[0-9]+)/$', views.api_player_detail, name='api_player_detail'),
-
+    url(r'^api/fields/(?P<field_id>[0-9]+)/$', views.api_field_detail, name='api_field_detail'),
+    url(r'^api/matches/(?P<match_id>[0-9]+)/$', views.api_match_detail, name='api_match_detail'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,6 @@
+import json
+import datetime
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, render
 
@@ -45,13 +48,81 @@ def player_detail(request, player_id):
     return render(request, 'main/player_detail.html', {'player': player})
 
 def api_players(request):
-    players = Player.objects.all()
-    data = []
-    for player in players:
-        data.append({"id": player.id, "name": player.name})
-    return JsonResponse({"players": data})
+    if request.method == "GET":
+        players = Player.objects.all()
+        data = []
+        for player in players:
+            data.append({"id": player.id, "name": player.name})
+        return JsonResponse({"players": data})
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        player = Player.objects.create(name=data["name"])
+        return JsonResponse({"id": player.id, "name": player.name})
+
+def api_fields(request):
+    if request.method == "GET":
+        fields = Field.objects.all()
+        data = []
+        for field in fields:
+            data.append({"id": field.id, "name": field.name})
+        return JsonResponse({"fields": data})
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        field = Field.objects.create(name=data["name"])
+        return JsonResponse({"id": field.id, "name": field.name})
+
+def api_matches(request):
+    if request.method == "GET":
+        matches = Match.objects.all()
+        data = []
+        for match in matches:
+            data.append({"id": match.id, "date_and_time": match.date_and_time,
+                         "player1": match.player1.name, "player2": match.player2.name,
+                         "player1_score": match.player1_score, "player2_score": match.player2_score,
+                         "field": match.field.name})
+        return JsonResponse({"matches": data})
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        #if the player entered already exists, use that object. If not, make a new one and use that
+        #some kind of try catch block would be good here. Try to "get(name=data["player1"])" if that throws an exception
+        #then make a new player and use that
+
+        #bad part about this is that it has to be EXACTLY the same name
+        #also cannot identify by id number
+        #there is definitely a better way to do this. Maybe some django magic?
+        if Player.objects.filter(name=data["player1"]).exists():
+            p1 = Player.objects.get(name=data["player1"])
+        else:
+            p1 = Player.objects.create(name=data["player1"])
+
+        if Player.objects.filter(name=data["player2"]).exists():
+            p2 = Player.objects.get(name=data["player2"])
+        else:
+            p2 = Player.objects.create(name=data["player2"])
+
+        if Field.objects.filter(name=data["field"]).exists():
+            field = Field.objects.get(name=data["field"])
+        else:
+            field = Field.objects.create(name=data["field"])
+        match = Match.objects.create(date_and_time=datetime.datetime.now(),player1=p1,player2=p2,
+                                     player1_score=data["player1_score"],player2_score=data["player2_score"],field=field)
+        return JsonResponse({"id": match.id, "date_and_time": match.date_and_time,
+                             "player1": match.player1.name, "player2": match.player2.name,
+                             "player1_score": match.player1_score, "player2_score": match.player2_score,
+                             "field": match.field.name})
 
 def api_player_detail(request, player_id):
     player = get_object_or_404(Player, pk=player_id)
     return JsonResponse({"id": player.id, "name": player.name})
+
+def api_field_detail(request, field_id):
+    field = get_object_or_404(Field, pk=field_id)
+    return JsonResponse({"id": field.id, "name": field.name})
+
+def api_match_detail(request, match_id):
+    match = get_object_or_404(Match, pk=match_id)
+    return JsonResponse({"id": match.id, "date_and_time": match.date_and_time,
+                         "player1": match.player1.name, "player2": match.player2.name,
+                         "player1_score": match.player1_score, "player2_score": match.player2_score,
+                         "field": match.field.name})
 
